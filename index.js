03a8480c719346d7359a33b7ba3e10f2a4b7fcde
Kameron Lutes <Kameron Lutes>
2016-12-04 22:07:19 -0500

    Added typeracer stuff

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var cfenv = require('cfenv');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var appEnv = cfenv.getAppEnv();
+http.listen(appEnv.port || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
