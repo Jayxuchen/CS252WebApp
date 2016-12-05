a1854389a70ba7c43e5c42ea1ca6aa41057dcba4
Kameron Lutes <Kameron Lutes>
2016-12-05 03:46:35 -0500

    Incorporated matt's code

--- index.js
+++ index.js
@@ -8,14 +8,21 @@ http.listen(appEnv.port || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-
+users = [];
+var roomnumber = 1;
 
 io.on('connection', function(socket){
+  if(io.nsps['/'].adapter.rooms['room-'+roomnumber] && Object.keys(io.nsps['/'].adapter.rooms['room-'+roomnumber]).length > 1){
+  	roomnumber++;
+  }
+  socket.join('room-'+roomnumber);
+  console.log('room-'+roomnumber);
+
   socket.on('chat message', function(msg){
-    socket.broadcast.emit('chat message', msg);
+    socket.broadcast.to(socket.rooms[socket.rooms.length - 1]).emit('chat message', msg);
   });
 
   socket.on('winner', function(){
-  	socket.broadcast.emit('winner', '');
+  	socket.broadcas.to(socket.rooms[socket.rooms.length - 1]).emit('winner', '');
   });
 });
--- localindex.js
+++ localindex.js
@@ -6,13 +6,22 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
+users = [];
+var roomnumber = 1;
+
 io.on('connection', function(socket){
+  if(io.nsps['/'].adapter.rooms['room-'+roomnumber] && Object.keys(io.nsps['/'].adapter.rooms['room-'+roomnumber]).length > 1){
+  	roomnumber++;
+  }
+  socket.join('room-'+roomnumber);
+  console.log('room-'+roomnumber);
+
   socket.on('chat message', function(msg){
-    socket.broadcast.emit('chat message', msg);
+    socket.broadcast.to(socket.rooms[socket.rooms.length - 1]).emit('chat message', msg);
   });
 
   socket.on('winner', function(){
-  	socket.broadcast.emit('winner', '');
+  	socket.broadcas.to(socket.rooms[socket.rooms.length - 1]).emit('winner', '');
   });
 });
 
